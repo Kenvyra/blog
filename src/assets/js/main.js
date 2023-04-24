f208851fde836fcbe435dff7021a9dbe991becd9
Jens Reidel <adrian@travitia.xyz>
2023-04-25 00:27:11 +0200

    Make the download button work
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,3 +7,35 @@ window.addEventListener("load", () => {
         navigator.serviceWorker.register("/sw.js");
     }
 });
+
+const device = document.getElementById("device");
+const flavor = document.getElementById("flavor");
+const downloadButton = document.getElementById("download");
+
+device.addEventListener("input", updateDownloadButton);
+flavor.addEventListener("input", updateDownloadButton);
+
+updateDownloadButton();
+
+async function updateDownloadButton() {
+    if (device.value.length > 0 && flavor.value.length > 0) {
+        downloadButton.textContent = "Preparing download...";
+
+        try {
+            const response = await fetch(`https://raw.githubusercontent.com/Kenvyra/ota/kenvyra-13.0/${device.value}_${flavor.value}.json`);
+            const json = await response.json();
+
+            downloadButton.href = json.response[0].url;
+            downloadButton.classList.remove("is-disabled");
+            downloadButton.classList.add("is-active");
+
+            downloadButton.textContent = "Download";
+        } catch (e) {
+            downloadButton.textContent = "No build available yet";
+        }
+    } else {
+        downloadButton.classList.remove("is-active");
+        downloadButton.classList.add("is-disabled");
+        downloadButton.textContent = "Select a device and flavor";
+    }
+}
